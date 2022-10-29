da2513de8c62f70b918dc8a9d47459c11e5de009
Kwakuafram <vidaforkuo557@gmail.com>
2022-10-28 12:05:31 -1200

    Django project

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,6 +1,19 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
-class Artiste(models.model):
+class Artiste(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+    age = models.IntegerField
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    date_release = models.DateTimeField
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+
+class Lyrics(models.Model):
+    content = models.CharField(max_length=500)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
